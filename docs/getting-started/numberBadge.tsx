47c51656f56bbdc619f5f5d68c08298360325adf
Shreyansh Deb <shreyanshdeb@SYTPLs-MacBook-Pro.local>
2024-02-21 14:47:26 +0530

    Add Number Badge

--- docs/getting-started/numberBadge.tsx
+++ docs/getting-started/numberBadge.tsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 type NumberBadgeProps = {
   number: number;
+  color: string;
 };
 
-const NumberBadge: React.FC<NumberBadgeProps> = ({ number }) => (
-  <div style={{
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '40px', // Ensures the badge is wide enough for its content
-    height: '40px', // Same as width to ensure the badge is circular
-    backgroundColor: '#007bff',
-    color: '#ffffff',
-    fontWeight: 'bold',
-    borderRadius: '50%',
-    fontSize: '16px',
-  }}>
-    {number}
-  </div>
-);
+const NumberBadge: React.FC<NumberBadgeProps> = ({ number=0, color = '#bbb' }) => {
+  // Default color is #bbb if none is provided
+  const badgeStyle = {
+    backgroundColor: color,
+    // Add other styles here if needed
+  };
 
-export default NumberBadge;
+  return (
+    <div className="dot-wrapper">
+      <div className="dot" style={badgeStyle}>
+        {number}
+      </div>
+    </div>
+  );
+};
+
+export  { NumberBadge };
