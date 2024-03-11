50aa16ea3155d57c5e382b03d24700ea2b9cd458
Shreyansh Deb <shreyanshdeb@SYTPLs-MacBook-Pro.local>
2024-03-11 20:47:45 +0530

    Create multi level sidebar

--- sidebars.js
+++ sidebars.js
@@ -9,6 +9,153 @@
  Create as many sidebars as you want.
  */
 
+const build = [
+{
+          type: 'category', 
+          label: 'Self Serve',
+          //link: {
+          //  type: 'generated-index',
+          //  title: 'Self Serve',
+          //  description: "Self Serve Documentation",
+          //  keywords: ["self-serve"],
+          //},
+          items: [
+            'getting-started/self-serve/overview',
+            'getting-started/self-serve/start-here',
+            {
+              type: 'category',
+              label: 'Customize',
+              items: [
+                'getting-started/self-serve/customize/http_trigger',
+                'getting-started/self-serve/customize/authentication',
+                'getting-started/self-serve/customize/enrichment',
+                'getting-started/self-serve/customize/waitlist',
+                'getting-started/self-serve/customize/onboarding',
+                'getting-started/self-serve/customize/store_leads',
+                'getting-started/self-serve/customize/pricing',
+                'getting-started/self-serve/customize/create_tenant',
+                'getting-started/self-serve/customize/notification',
+                'getting-started/self-serve/customize/redirection',
+              ],
+            },
+            {
+              type: 'category',
+              label: 'Integrate',
+              items: [
+                'getting-started/self-serve/integrate/placeholder',
+              ]
+            },
+            {
+              type: 'category',
+              label: 'Go Live',
+              items: [
+                'getting-started/self-serve/go-live/placeholder',
+              ]
+            },
+            {
+              type: 'category',
+              label: 'APIs',
+              items: [
+                'getting-started/self-serve/apis/intro',
+                'getting-started/self-serve/apis/triggers',
+                'getting-started/self-serve/apis/invitation',
+                'getting-started/self-serve/apis/onboarding',
+                'getting-started/self-serve/apis/publish',
+                'getting-started/self-serve/apis/promote',
+              ]
+            },
+            //'getting-started/self-serve/self-serve',
+            //'getting-started/self-serve/workflow-canvas',
+            {
+              type: 'category',
+              label: 'Features',
+              items: [
+                "getting-started/self-serve/features/no_code_orchestration"
+              ]
+            }
+          ]
+        }
+]
+const analyze = [
+{
+          type: 'category', 
+          label: 'Analyze',
+          //link: {
+          //  type: 'generated-index',
+          //  title: 'Analyze',
+          //  description: "Get started with ThriveStack's Analyze Module",
+          //  keywords: ['analyze'],
+          //},
+          items: [
+          //"getting-started/analyze/intro", // Delete this, this is empty
+          'getting-started/analyze/instrumentation/overview',
+          {
+              type: 'category',
+              label: 'APIs',
+              items: [
+                "getting-started/analyze/authentication",
+                'getting-started/analyze/instrumentation/identification/user',
+                'getting-started/analyze/instrumentation/identification/group',
+                'getting-started/analyze/instrumentation/events/event-tracking',
+                'getting-started/analyze/instrumentation/page',
+              ],
+          },
+          {
+            type: 'category', 
+            label: 'Events',
+            items: [
+            'getting-started/analyze/instrumentation/events/standard/signed_up',
+            'getting-started/analyze/instrumentation/events/standard/account_created',
+            'getting-started/analyze/instrumentation/events/standard/account_added_user',
+            'getting-started/analyze/instrumentation/events/standard/signed_in',
+            'getting-started/analyze/instrumentation/events/standard/signed_out',
+            'getting-started/analyze/instrumentation/events/standard/invite_sent',
+            'getting-started/analyze/instrumentation/events/standard/trial_started',
+            'getting-started/analyze/instrumentation/events/standard/trial_ended',
+            'getting-started/analyze/instrumentation/events/standard/account_removed_user',
+            'getting-started/analyze/instrumentation/events/standard/account_deleted',
+            {
+              type: 'category', 
+              label: 'Custom',
+              link: {
+                type: 'generated-index',
+                title: 'Custom',
+                description: 'Custom Docs',
+                keywords: ['custom-events'],
+              },
+              items: [
+              ]
+            },
+            ]
+          },
+
+            {
+              type: 'category', 
+              label: 'Reports',
+              items: [
+                'getting-started/analyze/reports/overview',
+                'getting-started/analyze/reports/standard/acquisition',
+                //'getting-started/analyze/reports/standard/activation',
+                //'getting-started/analyze/reports/standard/retention',
+                //'getting-started/analyze/reports/standard/engagement',
+                //'getting-started/analyze/reports/standard/expansion',
+                {
+                  type: 'category', 
+                  label: 'Custom',
+                  link: {
+                    type: 'generated-index',
+                    title: 'Custom',
+                    description: 'Custom Reports Docs',
+                    keywords: ['custom-reports'],
+                  },
+                  items: [
+                  ]
+                },
+              ]
+            },
+          ]
+        }
+]
 
 const dev = [
     {
@@ -172,152 +319,13 @@ const sidebars = {
         //  },
         //  items: [...dev, ...pms, ...gtm],
         //},
-        {
-          type: 'category', 
-          label: 'Self Serve',
-          link: {
-            type: 'generated-index',
-            title: 'Self Serve',
-            description: "Self Serve Documentation",
-            keywords: ["self-serve"],
-          },
-          items: [
-            'getting-started/self-serve/overview',
-            'getting-started/self-serve/start-here',
-            {
-              type: 'category',
-              label: 'Customize',
-              items: [
-                'getting-started/self-serve/customize/http_trigger',
-                'getting-started/self-serve/customize/authentication',
-                'getting-started/self-serve/customize/enrichment',
-                'getting-started/self-serve/customize/waitlist',
-                'getting-started/self-serve/customize/onboarding',
-                'getting-started/self-serve/customize/store_leads',
-                'getting-started/self-serve/customize/pricing',
-                'getting-started/self-serve/customize/create_tenant',
-                'getting-started/self-serve/customize/notification',
-                'getting-started/self-serve/customize/redirection',
-              ],
-            },
-            {
-              type: 'category',
-              label: 'Integrate',
-              items: [
-                'getting-started/self-serve/integrate/placeholder',
-              ]
-            },
-            {
-              type: 'category',
-              label: 'Go Live',
-              items: [
-                'getting-started/self-serve/go-live/placeholder',
-              ]
-            },
-            {
-              type: 'category',
-              label: 'APIs',
-              items: [
-                'getting-started/self-serve/apis/intro',
-                'getting-started/self-serve/apis/triggers',
-                'getting-started/self-serve/apis/invitation',
-                'getting-started/self-serve/apis/onboarding',
-                'getting-started/self-serve/apis/publish',
-                'getting-started/self-serve/apis/promote',
-              ]
-            },
-            //'getting-started/self-serve/self-serve',
-            //'getting-started/self-serve/workflow-canvas',
-            {
-              type: 'category',
-              label: 'Features',
-              items: [
-                "getting-started/self-serve/features/no_code_orchestration"
-              ]
-            }
-          ]
-        },
-        {
-          type: 'category', 
-          label: 'Analyze',
-          //link: {
-          //  type: 'generated-index',
-          //  title: 'Analyze',
-          //  description: "Get started with ThriveStack's Analyze Module",
-          //  keywords: ['analyze'],
-          //},
-          items: [
-          //"getting-started/analyze/intro", // Delete this, this is empty
-          'getting-started/analyze/instrumentation/overview',
-          {
-              type: 'category',
-              label: 'APIs',
-              items: [
-                "getting-started/analyze/authentication",
-                'getting-started/analyze/instrumentation/identification/user',
-                'getting-started/analyze/instrumentation/identification/group',
-                'getting-started/analyze/instrumentation/events/event-tracking',
-                'getting-started/analyze/instrumentation/page',
-              ],
-          },
-          {
-            type: 'category', 
-            label: 'Events',
-            items: [
-            'getting-started/analyze/instrumentation/events/standard/signed_up',
-            'getting-started/analyze/instrumentation/events/standard/account_created',
-            'getting-started/analyze/instrumentation/events/standard/account_added_user',
-            'getting-started/analyze/instrumentation/events/standard/signed_in',
-            'getting-started/analyze/instrumentation/events/standard/signed_out',
-            'getting-started/analyze/instrumentation/events/standard/invite_sent',
-            'getting-started/analyze/instrumentation/events/standard/trial_started',
-            'getting-started/analyze/instrumentation/events/standard/trial_ended',
-            'getting-started/analyze/instrumentation/events/standard/account_removed_user',
-            'getting-started/analyze/instrumentation/events/standard/account_deleted',
-            {
-              type: 'category', 
-              label: 'Custom',
-              link: {
-                type: 'generated-index',
-                title: 'Custom',
-                description: 'Custom Docs',
-                keywords: ['custom-events'],
-              },
-              items: [
-              ]
-            },
-            ]
-          },
-
-            {
-              type: 'category', 
-              label: 'Reports',
-              items: [
-                'getting-started/analyze/reports/overview',
-                'getting-started/analyze/reports/standard/acquisition',
-                //'getting-started/analyze/reports/standard/activation',
-                //'getting-started/analyze/reports/standard/retention',
-                //'getting-started/analyze/reports/standard/engagement',
-                //'getting-started/analyze/reports/standard/expansion',
-                {
-                  type: 'category', 
-                  label: 'Custom',
-                  link: {
-                    type: 'generated-index',
-                    title: 'Custom',
-                    description: 'Custom Reports Docs',
-                    keywords: ['custom-reports'],
-                  },
-                  items: [
-                  ]
-                },
-              ]
-            },
-          ]
-        },
+        build,
+        analyze,
       ]
     }
   ],
+  buildSidebar: build,
+  analyzeSidebar: analyze,
   devsSidebar: dev,
   pmsSidebar: pms,
   gtmLeadersSidebar: gtm,
